448c5f27abbb32d872dcedb2dc98a63c52389fd8
emregozdamla <madenciemre01@gmail.com>
2023-07-13 17:28:12 +0300

    kronometre ilk versiyon winforms ile

---
+++ Kronometre--Projesi/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Kronometre__Projesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(90, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(50, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(180, 75);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(50, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(270, 75);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(50, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(360, 75);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(50, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(270, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Durdur";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(360, 151);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Sıfırla";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(270, 113);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Değer Gir";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(90, 113);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(41, 17);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "İleri";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(179, 113);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(44, 17);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Geri";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 16;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(90, 151);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(140, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Değerleri Gir";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(270, 151);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(140, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Değer Girmekten Vazgeç";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Visible = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 200);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Kronometre";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Kronometre--Projesi/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kronometre__Projesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int salise = 0;
+        int saniye = 0;
+        int dakika = 0;
+        int saat = 0;
+        string saliseText = "00";
+        string saniyeText = "00";
+        string dakikaText = "00";
+        string saatText = "00";
+        
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "00";
+            textBox2.Text = "00";
+            textBox3.Text = "00";
+            textBox4.Text = "00";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(radioButton1.Checked==true && radioButton2.Checked==false)
+            {
+                salise++;
+                if (salise == 60) { salise = 0;saniye++; }
+                if (saniye == 60) { saniye = 0;dakika++; }
+                if (dakika == 60) { dakika = 0;saat++; }
+                if (salise < 10) saliseText = "0" + salise;
+                else if (salise >= 10) saliseText = ""+salise;
+                if (saniye < 10) saniyeText = "0" + saniye;
+                else if (saniye >= 10) saniyeText = "" + saniye;
+                if (dakika < 10) dakikaText = "0" + dakika;
+                else if (dakika >= 10) dakikaText = "" + dakika;
+                if (saat < 10) saatText = "0" + saat;
+                else if (saat >= 10) saatText = "" + saat;
+                textBox4.Text = saliseText;
+                textBox3.Text = saniyeText;
+                textBox2.Text = dakikaText;
+                textBox1.Text = saatText;
+
+
+            }
+            else if(radioButton1.Checked==false&&radioButton2.Checked==true)
+            {
+                salise--;
+                if (salise == -1) { salise = 59;saniye--;}
+                if (saniye == -1) { saniye = 59;dakika--; }
+                if (dakika == -1) { dakika = 59;saat--; }
+                if (salise < 10) saliseText = "0" + salise;
+                else if (salise >= 10) saliseText = "" + salise;
+                if (saniye < 10) saniyeText = "0" + saniye;
+                else if (saniye >= 10) saniyeText = "" + saniye;
+                if (dakika < 10) dakikaText = "0" + dakika;
+                else if (dakika >= 10) dakikaText = "" + dakika;
+                if (saat < 10) saatText = "0" + saat;
+                else if (saat >= 10) saatText = "" + saat;
+                textBox4.Text = saliseText;
+                textBox3.Text = saniyeText;
+                textBox2.Text = dakikaText;
+                textBox1.Text = saatText;
+                if (saat == 0 && dakika == 0 && saniye == 0 && salise == 0) timer1.Stop();
+
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            button1.Text = "Başla";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            button1.Text = "Devam Et";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            salise = 0;
+            saniye = 0;
+            dakika = 0;
+            saat = 0;
+            textBox1.Text = "00";
+            textBox2.Text = "00";
+            textBox3.Text = "00";
+            textBox4.Text = "00";
+            timer1.Stop();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = false;
+            radioButton1.Visible = false;
+            radioButton2.Visible = false;
+
+            button5.Visible = true;
+            button6.Visible = true;
+
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
+            textBox4.Enabled = true;
+
+            timer1.Stop();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = true;
+            radioButton1.Visible = true;
+            radioButton2.Visible = true;
+
+            button5.Visible = false;
+            button6.Visible = false;
+
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+            textBox4.Enabled = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                salise = Int32.Parse(textBox4.Text);
+                saniye = Int32.Parse(textBox3.Text);
+                dakika = Int32.Parse(textBox2.Text);
+                saat = Int32.Parse(textBox1.Text);
+                if (salise > 59 || salise < 0 || saniye > 59 || saniye < 0 || dakika > 59 || dakika < 0 || saat < 0)
+                    Convert.ToInt32("abc");
+
+                button1.Visible = true;
+                button2.Visible = true;
+                button3.Visible = true;
+                button4.Visible = true;
+                radioButton1.Visible = true;
+                radioButton2.Visible = true;
+
+                button5.Visible = false;
+                button6.Visible = false;
+
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox3.Enabled = false;
+                textBox4.Enabled = false;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Girdiğiniz değerlerde hata var", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
